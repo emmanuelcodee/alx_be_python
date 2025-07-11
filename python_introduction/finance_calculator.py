5b0bc689d86be0d5836665bd5c1c4f93ec122848
emmanuelcodee <edwiinemmanuell@gmail.com>
2025-07-11 02:39:59 +0100

    converted  savings to whole number

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,11 @@
-income = int(input("Enter your monthly income"))
-expenses = int(input("Enter your total monthly expenses"))
+income = int(input("Enter your monthly income:"))
+expenses = int(input("Enter your total monthly expenses:"))
 
 savings = income - expenses
 
 interest_rate = 0.05
 
-projected_savings = savings * 12 + (savings * 12 * interest_rate)
+projected_savings = int(savings * 12 + (savings * 12 * interest_rate))
 
 print("Your monthly savings are $",savings,)
 print("Projected savings after one year, with interest, is $",projected_savings)
