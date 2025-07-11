5e4e8af8e6beb73a1b9f3c01c0ed271aad07ecfd
emmanuelcodee <edwiinemmanuell@gmail.com>
2025-07-11 02:41:50 +0100

    semicolon

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -8,4 +8,4 @@ interest_rate = 0.05
 projected_savings = int(savings * 12 + (savings * 12 * interest_rate))
 
 print("Your monthly savings are $",savings,)
-print("Projected savings after one year, with interest, is $",projected_savings)
+print("Projected savings after one year, with interest, is:",projected_savings)
