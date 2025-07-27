06586b9b7d28ad7040d94f36f5eb81cb07ee860d
emmanuelcodee <edwiinemmanuell@gmail.com>
2025-07-27 22:41:43 +0100

    familiarize myself with datetime

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,4 +1,4 @@
-
+shopping_list = []
 def display_menu():
     print("\n=== Shopping List Manager ===")
     print("1. Add an item")
@@ -6,7 +6,7 @@ def display_menu():
     print("3. View shopping list")
     print("4. Exit")
 
-def add_item(shopping_list):
+def add_item():
     item = input("Enter the name of the item to add: ").strip()
     if item:
         shopping_list.append(item)
@@ -14,7 +14,7 @@ def add_item(shopping_list):
     else:
         print("Item name cannot be empty.")
 
-def remove_item(shopping_list):
+def remove_item():
     item = input("Enter the name of the item to remove: ").strip()
     if item in shopping_list:
         shopping_list.remove(item)
@@ -22,7 +22,7 @@ def remove_item(shopping_list):
     else:
         print(f"'{item}' is not in your shopping list.")
 
-def view_list(shopping_list):
+def view_list():
     if shopping_list:
         print("\nYour Shopping List:")
         for i, item in enumerate(shopping_list, start=1):
