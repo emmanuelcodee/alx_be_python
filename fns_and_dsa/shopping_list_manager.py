e2284c24e36f5bb851db29cdc29bfdfd3f416d1c
emmanuelcodee <edwiinemmanuell@gmail.com>
2025-07-27 22:10:30 +0100

    implemented a shopping list manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,32 @@
+
+def display_menu():
+    print("\n=== Shopping List Manager ===")
+    print("1. Add an item")
+    print("2. Remove an item")
+    print("3. View shopping list")
+    print("4. Exit")
+
+def add_item(shopping_list):
+    item = input("Enter the name of the item to add: ").strip()
+    if item:
+        shopping_list.append(item)
+        print(f"'{item}' has been added to your shopping list.")
+    else:
+        print("Item name cannot be empty.")
+
+def remove_item(shopping_list):
+    item = input("Enter the name of the item to remove: ").strip()
+    if item in shopping_list:
+        shopping_list.remove(item)
+        print(f"'{item}' has been removed from your shopping list.")
+    else:
+        print(f"'{item}' is not in your shopping list.")
+
+def view_list(shopping_list):
+    if shopping_list:
+        print("\nYour Shopping List:")
+        for i, item in enumerate(shopping_list, start=1):
+            print(f"{i}. {item}")
+    else:
+        print("Your shopping list is currently empty.")
+
